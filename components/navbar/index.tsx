f8133867fcb6f581fe8a36af369016546d8f3264
jjk <2361491843@qq.com>
2023-02-12 15:51:16 +0800

    新增tab和修改部分global.scss

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,9 +1,11 @@
-import { FC, useContext, useRef } from "react";
+import { FC, useContext, useRef, useEffect } from "react";
 import styles from "./styles.module.scss";
 import { ThemeContext } from "@/stores/theme";
 import { UserAgentContext } from "@/stores/userAgent";
 import { Themes, Environment } from "@/constants/enum";
 import { Popup, IPopupRef } from "../popup";
+import { Topbar } from "./topbar";
+import { LabelBar } from "./labelbar";
 
 export interface INavBarProps {}
 
@@ -12,43 +14,27 @@ export const NavBar: FC<INavBarProps> = ({}) => {
   const { userAgent } = useContext(UserAgentContext);
   const popupRef = useRef<IPopupRef>(null);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop;
+      const topbar = document.querySelector("#topbar");
+
+      if (scrollTop > 385) {
+        topbar && topbar.classList.add(styles.hiddenTopbar);
+      } else {
+        if (topbar && topbar.classList.contains(styles.hiddenTopbar)) {
+          topbar && topbar.classList.remove(styles.hiddenTopbar);
+        }
+      }
+    };
+    window.addEventListener("scroll", handleScroll, true);
+  }, []);
   return (
-    <div className={styles.navBar}>
-      <a href="http://127.0.0.1:3000/">
-        <div className={styles.logoIcon}></div>
-      </a>
-      <div className={styles.themeArea}>
-        <div
-          className={styles.popupText}
-          onClick={(): void => {
-            popupRef.current?.open();
-          }}
-        >
-          弹窗示范
-        </div>
-        {userAgent === Environment.pc && (
-          <span className={styles.text}>当前是pc端样式</span>
-        )}
-        {userAgent === Environment.ipad && (
-          <span className={styles.text}>当前是Ipad端样式</span>
-        )}
-        {userAgent === Environment.mobile && (
-          <span className={styles.text}>当前是移动端样式</span>
-        )}
-        <div
-          className={styles.themeIcon}
-          onClick={(): void => {
-            if (localStorage.getItem("theme") === Themes.light) {
-              setTheme(Themes.dark);
-            } else {
-              setTheme(Themes.light);
-            }
-          }}
-        ></div>
-      </div>
-      <Popup ref={popupRef}>
-        <div>这是一个弹窗</div>
-      </Popup>
+    <div style={{ position: "relative" }}>
+      <header className={styles.con} id="navbar">
+        <Topbar></Topbar>
+        <LabelBar></LabelBar>
+      </header>
     </div>
   );
 };
